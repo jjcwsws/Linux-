5e862b7897ee00b2d83122768e7295ca33f282b9
Xianen Ling <112937740+jjcwsws@users.noreply.github.com>
2024-09-17 08:02:30 +0800

    Add files via upload

---
+++ lesson4_9.17/test.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int Sum(int n)
+{
+    int flag = 1;
+
+    if(flag == 1) printf("haha\n");
+    else printf("hehe\n");
+
+    int i = 1;
+    int sum = 0;
+    for(; i<sum; i++)
+    {
+        sum += i;
+    }
+
+    return sum;
+}
+
+int main()
+{
+    printf("hello world!\n");
+    printf("hello world!\n");
+    printf("hello world!\n");
+    printf("hello world!\n");
+    printf("hello world!\n");
+    printf("hello world!\n");
+    printf("hello world!\n");
+    printf("hello world!\n");
+    printf("hello world!\n");
+    printf("hello world!\n");
+    printf("hello world!\n");
+    printf("hello world!\n");
+    printf("hello world!\n");
+    printf("hello world!\n");
+    printf("hello world!\n");
+    printf("hello world!\n");
+    printf("hello world!\n");
+    printf("hello world!\n");
+    printf("hello world!\n");
+    printf("hello world!\n");
+    printf("hello world!\n");
+    printf("hello world!\n");
+    printf("hello world!\n");
+    printf("hello world!\n");
+    printf("hello world!\n");
+
+    int result = Sum(100);
+
+    printf("result = %d\n", result);
+
+    return 0;
+}
